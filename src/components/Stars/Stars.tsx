c1dc7b92c1dd7dfc57ee4ca070ce600ed2f49cb8
Diana Verevkina <diana_verevkina@mail.ru>
2023-11-16 01:42:04 +0300

    initial commit

---
+++ src/components/Stars/Stars.tsx
@@ -0,0 +1,20 @@
+import { Star } from "./Star/Star";
+
+interface IStars {
+  count: number,
+}
+
+export const Stars = ({ count }: IStars) => {
+  if (count < 1 || count > 5 || typeof count !== 'number') return;
+
+  const stars = [];
+  for (let i = 0; i < count; i++) {
+    const star = <Star />;
+    stars.push(star);
+  }
+  return (
+    <ul className="card-body-stars u-clearfix">
+      {stars}
+    </ul>
+  );
+}
